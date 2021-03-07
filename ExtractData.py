e31a137400c844cdee9a6f9e2c1f020ca90681cb
VaseSimion <42439265+VaseSimion@users.noreply.github.com>
2021-03-07 12:35:03 +0100

    Data processing added to get what I am focused on

--- ExtractData.py
+++ ExtractData.py
@@ -1,6 +1,7 @@
 from urllib.request import urlopen
 import json
 import configparser
+from datetime import datetime
 
 config = configparser.ConfigParser()
 config.read('Settings.ini')
@@ -12,18 +13,82 @@ def return_income(stock):
           "?limit=800&apikey=" + key
     response = urlopen(url)
     income = json.loads(response.read().decode("utf-8"))
+    income.reverse()
     return income
+
 
 def return_balance_sheet(stock):
     url = "https://financialmodelingprep.com/api/v3/" + "balance-sheet" + "-statement/" + stock + \
           "?limit=800&apikey=" + key
     response = urlopen(url)
     balance = json.loads(response.read().decode("utf-8"))
+    balance.reverse()
     return balance
+
 
 def return_cash_flow(stock):
     url = "https://financialmodelingprep.com/api/v3/" + "cash-flow" + "-statement/" + stock + \
           "?limit=800&apikey=" + key
     response = urlopen(url)
     cash = json.loads(response.read().decode("utf-8"))
+    cash.reverse()
     return cash
+
+
+def return_stock_info(stock):
+    url = "https://financialmodelingprep.com/api/v3/" + "quote/" + stock + "?apikey=" + key
+    response = urlopen(url)
+    return json.loads(response.read().decode("utf-8"))[0]
+
+
+def return_processed_data(income, balance, cash, info):
+    # data for output
+    dates = []
+    free_cash_flow = []
+    net_income = []
+    revenue = []
+    current_equity = []
+    stockholder_equity = []
+    total_debt = []
+    earnings_per_share = []
+    dividends_paid = []
+
+    print("cash")
+    for year in cash:
+        print(year)
+        dates.append(datetime.strptime(year['date'], "%Y-%m-%d"))
+        free_cash_flow.append(year['freeCashFlow'] / 1e6)
+        dividends_paid.append(year["dividendsPaid"] / 1e6)
+
+    print("income")
+    for year in income:
+        print(year)
+        net_income.append(year["netIncome"] / 1e6)
+        revenue.append(year["revenue"] / 1e6)
+        earnings_per_share.append(year["eps"])
+
+    print("balance")
+    for year in balance:
+        print(year)
+        current_equity.append(year["totalCurrentAssets"] / 1e6 - year["totalCurrentLiabilities"] / 1e6)
+        stockholder_equity.append(year["totalStockholdersEquity"] / 1e6)
+        total_debt.append(year["totalDebt"] / 1e6)
+
+    # calculating derived ones
+    return_on_equity = [a / b for (a, b) in zip(net_income, stockholder_equity)]
+    profit_margin = [a / b for (a, b) in zip(net_income, revenue)]
+    dividends_per_share = [-x * 1e6 / info["sharesOutstanding"] for x in dividends_paid]
+
+    output_dictionary = {"Dates": dates,
+                         "FreeCashFlow": free_cash_flow,
+                         "NetIncome": net_income,
+                         "Revenue": revenue,
+                         "CurrentEquity": current_equity,
+                         "ShareholderEquity": stockholder_equity,
+                         "Debt": total_debt,
+                         "EPS": earnings_per_share,
+                         "Dividends": dividends_paid,
+                         "ROE": return_on_equity,
+                         "ProfitMargin": profit_margin,
+                         "DividendsPerShare": dividends_per_share}
+    return output_dictionary
--- GraphFunctions.py
+++ GraphFunctions.py
@@ -1,13 +1,15 @@
 import numpy as np
 import tulipy as ti
 import matplotlib
-matplotlib.use('Agg')
+#matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
+import yfinance as yf
+from datetime import  datetime
 
-
-def draw_macd_buy(stock, name):
+def save_macd_buy(stock_name):
+    stock = yf.download(tickers=stock_name, interval="1d", period="2y", threads=True)
     closing_price_list = stock['Close'].tolist()
     date_list = list(stock.index)
 
@@ -17,37 +19,7 @@ def draw_macd_buy(stock, name):
 
     plt.figure(figsize=(15, 8))
     ax1 = plt.subplot(212)
-    ax2 = plt.subplot(211, sharex=ax1, title=name)
-    numberofactivedays = len(date_list[16:])
-    arrangeddates = np.arange(numberofactivedays)
-    ax1.plot(arrangeddates, macd1, 'r')
-    ax1.plot(arrangeddates, macd2, 'y')
-    ax1.bar(arrangeddates, macdhistogram)
-    ax1.plot(arrangeddates, [0] * len(date_list[16:]))
-    ax2.plot(arrangeddates, closing_price_list[16:])
-    ax2.plot(arrangeddates, sma, 'r')
-    ax1.set_xticks(np.append(arrangeddates[0::20], arrangeddates[-1]))
-    ax2.set_xticks(np.append(arrangeddates[0::20], arrangeddates[-1]))
-    ax1.set_xticklabels([date.strftime("%Y-%m-%d") for date in date_list[16:]][0::20] +
-                        [[date.strftime("%Y-%m-%d") for date in date_list[16:]][-1]])
-    ax2.set_xticklabels([date.strftime("%Y-%m-%d") for date in date_list[16:]][0::20] +
-                        [[date.strftime("%Y-%m-%d") for date in date_list[16:]][-1]])
-    ax1.tick_params(rotation=30)
-    ax2.tick_params(rotation=30)
-    plt.show()
-
-
-def save_macd_buy(stock, name):
-    closing_price_list = stock['Close'].tolist()
-    date_list = list(stock.index)
-
-    numpyclose = np.asarray(closing_price_list)
-    macd1, macd2, macdhistogram = ti.macd(numpyclose, 8, 17, 9)  # for buy signals it should be 8,17,9
-    sma = ti.sma(numpyclose, 17)
-
-    plt.figure(figsize=(15, 8))
-    ax1 = plt.subplot(212)
-    ax2 = plt.subplot(211, sharex=ax1, title=name)
+    ax2 = plt.subplot(211, sharex=ax1, title=stock_name)
     numberofactivedays = len(date_list[16:])
     arrangeddates = np.arange(numberofactivedays)
     ax1.plot(arrangeddates, macd1, 'r')
@@ -59,43 +31,27 @@ def save_macd_buy(stock, name):
     ax2.plot(arrangeddates, sma, 'r')
     ax2.set(ylabel="Price")
 
-    ax1.set_xticks(np.append(arrangeddates[0::20], arrangeddates[-1]))
-    ax2.set_xticks(np.append(arrangeddates[0::20], arrangeddates[-1]))
-    ax1.set_xticklabels([date.strftime("%Y-%m-%d") for date in date_list[16:]][0::20] +
+    ax1.set_xticks(np.append(arrangeddates[0::60], arrangeddates[-1]))
+    ax2.set_xticks(np.append(arrangeddates[0::60], arrangeddates[-1]))
+    ax1.set_xticklabels([date.strftime("%Y-%m-%d") for date in date_list[16:]][0::60] +
                         [[date.strftime("%Y-%m-%d") for date in date_list[16:]][-1]])
-    ax2.set_xticklabels([date.strftime("%Y-%m-%d") for date in date_list[16:]][0::20] +
+    ax2.set_xticklabels([date.strftime("%Y-%m-%d") for date in date_list[16:]][0::60] +
                         [[date.strftime("%Y-%m-%d") for date in date_list[16:]][-1]])
     ax1.tick_params(rotation=30)
     ax2.tick_params(rotation=30)
-    plt.savefig("Reports/Support Files For Pdf/" + name + ".png")
+    plt.savefig("Support Files/" + stock_name + "-price" + ".png")
     plt.close()
 
 
-def draw_macd_sell(stock, name):
-    closing_price_list = stock['Close'].tolist()
-    date_list = list(stock.index)
-
-    numpyclose = np.asarray(closing_price_list)
-    macd1, macd2, macdhistogram = ti.macd(numpyclose, 12, 26, 9)  # for buy signals it should be 8,17,9
-    sma = ti.sma(numpyclose, 26)
-
+def save_free_cash_flow(cash_flow):
+    dates = []
+    free_cash_flow = []
+    for year in cash_flow:
+        dates.append(datetime.strptime(year['date'], "%Y-%m-%d"))
+        free_cash_flow.append(year['freeCashFlow'] / 1e6)
     plt.figure(figsize=(15, 8))
-    ax1 = plt.subplot(212)
-    ax2 = plt.subplot(211, sharex=ax1, title=name)
-    numberofactivedays = len(date_list[25:])
-    arrangeddates = np.arange(numberofactivedays)
-    ax1.plot(arrangeddates, macd1, 'r')
-    ax1.plot(arrangeddates, macd2, 'y')
-    ax1.bar(arrangeddates, macdhistogram)
-    ax1.plot(arrangeddates, [0] * len(date_list[25:]))
-    ax2.plot(arrangeddates, closing_price_list[25:])
-    ax2.plot(arrangeddates, sma, 'r')
-    ax1.set_xticks(np.append(arrangeddates[0::20], arrangeddates[-1]))
-    ax2.set_xticks(np.append(arrangeddates[0::20], arrangeddates[-1]))
-    ax1.set_xticklabels([date.strftime("%Y-%m-%d") for date in date_list[25:]][0::20] +
-                        [[date.strftime("%Y-%m-%d") for date in date_list[25:]][-1]])
-    ax2.set_xticklabels([date.strftime("%Y-%m-%d") for date in date_list[25:]][0::20] +
-                        [[date.strftime("%Y-%m-%d") for date in date_list[25:]][-1]])
-    ax1.tick_params(rotation=30)
-    ax2.tick_params(rotation=30)
-    plt.show()
+    plt.plot(dates, free_cash_flow)
+    plt.legend(["free cash flow"])
+    plt.savefig("Support Files/freecash_flow.png")
+    plt.close()
+
